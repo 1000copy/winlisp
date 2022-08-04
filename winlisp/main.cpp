ec6e0a4e130f0c9f30223d46bb006df5ea990b3b
reco <1000copy@gmail.com>
2022-08-04 15:52:32 +0800

    DONE - sysmet.c to lisp

--- winlisp/main.cpp
+++ winlisp/main.cpp
@@ -6,167 +6,7 @@
 #include "metric.h"
 #include "listview.h"
 
-#define NUMLINES ((int) (sizeof sysmetrics / sizeof sysmetrics [0]))
 
-struct
-{
-    int     iIndex;
-    const char* szLabel;
-    const char* szDesc;
-}
-sysmetrics[] =
-{
-     SM_CXSCREEN,             "SM_CXSCREEN",
-                              "Screen width in pixels",
-     SM_CYSCREEN,             "SM_CYSCREEN",
-                              "Screen height in pixels",
-     SM_CXVSCROLL,            "SM_CXVSCROLL",
-                              "Vertical scroll width",
-     SM_CYHSCROLL,            "SM_CYHSCROLL",
-                              "Horizontal scroll height",
-     SM_CYCAPTION,            "SM_CYCAPTION",
-                              "Caption bar height",
-     SM_CXBORDER,             "SM_CXBORDER",
-                              "Window border width",
-     SM_CYBORDER,             "SM_CYBORDER",
-                              "Window border height",
-     SM_CXFIXEDFRAME,         "SM_CXFIXEDFRAME",
-                              "Dialog window frame width",
-     SM_CYFIXEDFRAME,         "SM_CYFIXEDFRAME",
-                              "Dialog window frame height",
-     SM_CYVTHUMB,             "SM_CYVTHUMB",
-                              "Vertical scroll thumb height",
-     SM_CXHTHUMB,             "SM_CXHTHUMB",
-                              "Horizontal scroll thumb width",
-     SM_CXICON,               "SM_CXICON",
-                              "Icon width",
-     SM_CYICON,               "SM_CYICON",
-                              "Icon height",
-     SM_CXCURSOR,             "SM_CXCURSOR",
-                              "Cursor width",
-     SM_CYCURSOR,             "SM_CYCURSOR",
-                              "Cursor height",
-     SM_CYMENU,               "SM_CYMENU",
-                              "Menu bar height",
-     SM_CXFULLSCREEN,         "SM_CXFULLSCREEN",
-                              "Full screen client area width",
-     SM_CYFULLSCREEN,         "SM_CYFULLSCREEN",
-                              "Full screen client area height",
-     SM_CYKANJIWINDOW,        "SM_CYKANJIWINDOW",
-                              "Kanji window height",
-     SM_MOUSEPRESENT,         "SM_MOUSEPRESENT",
-                              "Mouse present flag",
-     SM_CYVSCROLL,            "SM_CYVSCROLL",
-                              "Vertical scroll arrow height",
-     SM_CXHSCROLL,            "SM_CXHSCROLL",
-                              "Horizontal scroll arrow width",
-     SM_DEBUG,                "SM_DEBUG",
-                              "Debug version flag",
-     SM_SWAPBUTTON,           "SM_SWAPBUTTON",
-                              "Mouse buttons swapped flag",
-     SM_CXMIN,                "SM_CXMIN",
-                              "Minimum window width",
-     SM_CYMIN,                "SM_CYMIN",
-                              "Minimum window height",
-     SM_CXSIZE,               "SM_CXSIZE",
-                              "Min/Max/Close button width",
-     SM_CYSIZE,               "SM_CYSIZE",
-                              "Min/Max/Close button height",
-     SM_CXSIZEFRAME,          "SM_CXSIZEFRAME",
-                              "Window sizing frame width",
-     SM_CYSIZEFRAME,          "SM_CYSIZEFRAME",
-                              "Window sizing frame height",
-     SM_CXMINTRACK,           "SM_CXMINTRACK",
-                              "Minimum window tracking width",
-     SM_CYMINTRACK,           "SM_CYMINTRACK",
-                              "Minimum window tracking height",
-     SM_CXDOUBLECLK,          "SM_CXDOUBLECLK",
-                              "Double click x tolerance",
-     SM_CYDOUBLECLK,          "SM_CYDOUBLECLK",
-                              "Double click y tolerance",
-     SM_CXICONSPACING,        "SM_CXICONSPACING",
-                              "Horizontal icon spacing",
-     SM_CYICONSPACING,        "SM_CYICONSPACING",
-                              "Vertical icon spacing",
-     SM_MENUDROPALIGNMENT,    "SM_MENUDROPALIGNMENT",
-                              "Left or right menu drop",
-     SM_PENWINDOWS,           "SM_PENWINDOWS",
-                              "Pen extensions installed",
-     SM_DBCSENABLED,          "SM_DBCSENABLED",
-                              "Double-Byte Char Set enabled",
-     SM_CMOUSEBUTTONS,        "SM_CMOUSEBUTTONS",
-                              "Number of mouse buttons",
-     SM_SECURE,               "SM_SECURE",
-                              "Security present flag",
-     SM_CXEDGE,               "SM_CXEDGE",
-                              "3-D border width",
-     SM_CYEDGE,               "SM_CYEDGE",
-                              "3-D border height",
-     SM_CXMINSPACING,         "SM_CXMINSPACING",
-                              "Minimized window spacing width",
-     SM_CYMINSPACING,         "SM_CYMINSPACING",
-                              "Minimized window spacing height",
-     SM_CXSMICON,             "SM_CXSMICON",
-                              "Small icon width",
-     SM_CYSMICON,             "SM_CYSMICON",
-                              "Small icon height",
-     SM_CYSMCAPTION,          "SM_CYSMCAPTION",
-                              "Small caption height",
-     SM_CXSMSIZE,             "SM_CXSMSIZE",
-                              "Small caption button width",
-     SM_CYSMSIZE,             "SM_CYSMSIZE",
-                              "Small caption button height",
-     SM_CXMENUSIZE,           "SM_CXMENUSIZE",
-                              "Menu bar button width",
-     SM_CYMENUSIZE,           "SM_CYMENUSIZE",
-                              "Menu bar button height",
-     SM_ARRANGE,              "SM_ARRANGE",
-                              "How minimized windows arranged",
-     SM_CXMINIMIZED,          "SM_CXMINIMIZED",
-                              "Minimized window width",
-     SM_CYMINIMIZED,          "SM_CYMINIMIZED",
-                              "Minimized window height",
-     SM_CXMAXTRACK,           "SM_CXMAXTRACK",
-                              "Maximum draggable width",
-     SM_CYMAXTRACK,           "SM_CYMAXTRACK",
-                              "Maximum draggable height",
-     SM_CXMAXIMIZED,          "SM_CXMAXIMIZED",
-                              "Width of maximized window",
-     SM_CYMAXIMIZED,          "SM_CYMAXIMIZED",
-                              "Height of maximized window",
-     SM_NETWORK,              "SM_NETWORK",
-                              "Network present flag",
-     SM_CLEANBOOT,            "SM_CLEANBOOT",
-                              "How system was booted",
-     SM_CXDRAG,               "SM_CXDRAG",
-                              "Avoid drag x tolerance",
-     SM_CYDRAG,               "SM_CYDRAG",
-                              "Avoid drag y tolerance",
-     SM_SHOWSOUNDS,           "SM_SHOWSOUNDS",
-                              "Present sounds visually",
-     SM_CXMENUCHECK,          "SM_CXMENUCHECK",
-                              "Menu check-mark width",
-     SM_CYMENUCHECK,          "SM_CYMENUCHECK",
-                              "Menu check-mark height",
-     SM_SLOWMACHINE,          "SM_SLOWMACHINE",
-                              "Slow processor flag",
-     SM_MIDEASTENABLED,       "SM_MIDEASTENABLED",
-                              "Hebrew and Arabic enabled flag",
-     SM_MOUSEWHEELPRESENT,    "SM_MOUSEWHEELPRESENT",
-                              "Mouse wheel present flag",
-     SM_XVIRTUALSCREEN,       "SM_XVIRTUALSCREEN",
-                              "Virtual screen x origin",
-     SM_YVIRTUALSCREEN,       "SM_YVIRTUALSCREEN",
-                              "Virtual screen y origin",
-     SM_CXVIRTUALSCREEN,      "SM_CXVIRTUALSCREEN",
-                              "Virtual screen width",
-     SM_CYVIRTUALSCREEN,      "SM_CYVIRTUALSCREEN",
-                              "Virtual screen height",
-     SM_CMONITORS,            "SM_CMONITORS",
-                              "Number of monitors",
-     SM_SAMEDISPLAYFORMAT,    "SM_SAMEDISPLAYFORMAT",
-                              "Same color format flag"
-};
 
 
 PAINTSTRUCT* str_ps(std::string str) {
@@ -240,7 +80,12 @@ cell proc_gettextmetrics(const cells& c) {
     result.list.push_back(c3);
     return result;
 }
-
+cell proc_getsystemmetrics(const cells& c) {
+    long n = atoi(eval(c[0],&global_env).val.c_str());
+    long k = GetSystemMetrics(n);
+    cell result(String,std::to_string(k));    
+    return result;
+}
 cell proc_app(const cells& c) {
     return cell(Number, "0");
 }
@@ -344,30 +189,32 @@ cell proc_lv_setcolumns(const cells& c)
     memset(&LvCol, 0, sizeof(LvCol));
     LvCol.mask = LVCF_TEXT | LVCF_WIDTH | LVCF_SUBITEM;
     for (int i = 0; i < c[1].list.size(); i++) {
-        LvCol.cx = 100;
+        LvCol.cx = atoi(c[2].list[i].val.c_str());
         std::string c1 = c[1].list[i].val;
         LvCol.pszText = (LPSTR)c1.c_str();
         SendMessage(hList, LVM_INSERTCOLUMN, i, (LPARAM)&LvCol);
     }    
     return true_sym;
 }
+int kk = 0;
 cell proc_lv_appenditem(const cells& c)
 {
     HWND hList = str_hwnd(c[0].val);
     LVITEM LvItem;    
-    LVCOLUMN  LvCol;
     memset(&LvItem, 0, sizeof(LvItem)); // Zero struct's Members    
     LvItem.mask = LVIF_TEXT;   // Text Style
     LvItem.cchTextMax = 256; // Max size of test
-    LvItem.iItem = 0;          // choose item  
+    int index = SendMessage(hList, LVM_GETITEMCOUNT, 0, 0);//atoi(c[1].val.c_str());          // choose item  
+    LvItem.iItem = index;
     LvItem.iSubItem = 0;       // Put in first coluom
-    LvItem.pszText = (LPSTR)(c[1].list[0].val.c_str());
+    int base = 2;
+    LvItem.pszText =(LPSTR)(c[base].list[0].val.c_str());
     SendMessage(hList, LVM_INSERTITEM, 0, (LPARAM)&LvItem); // Send info to the Listview
 
-    for (int i = 1; i < c[1].list.size(); i++) // Add SubItems in a loop
+    for (int i = 1; i < c[base].list.size(); i++) // Add SubItems in a loop
     {
         LvItem.iSubItem = i;        
-        LvItem.pszText = (LPSTR)(c[1].list[i].val.c_str());
+        LvItem.pszText = (LPSTR)(c[base].list[i].val.c_str());
         SendMessage(hList, LVM_SETITEM, 0, (LPARAM)&LvItem); // Enter text to SubItems
     }    
     return true_sym;
@@ -660,6 +507,7 @@ void add_winglobals() {
     global_env["trap"] = cell(&proc_trap);
     global_env["eval"] = cell(&proc_eval);
     global_env["gettextmetrics"] = cell(&proc_gettextmetrics);
+    global_env["getsystemmetrics"] = cell(&proc_getsystemmetrics);
     global_env["lv_create"] = cell(&proc_lv_create);
     global_env["lv_setcolumns"] = cell(&proc_lv_setcolumns);
     global_env["lv_appenditem"] = cell(&proc_lv_appenditem);
---
+++ winlisp/main.lsp
@@ -0,0 +1,25 @@
+(begin
+    (load 'constants.lsp')    
+    (define onpaint(lambda (hwnd )
+        (begin
+            (define hdc (beginpaint hwnd ))
+            (define rect(getclientrect hwnd ))
+            (drawtext hdc '0099' rect)         
+            (line hdc 10 10 100 200)
+            (endpaint  hdc)
+            #t
+        )
+    ))
+    (define winproc         (lambda (hwnd msg )
+        (begin
+            (if (= WM_PAINT msg)(onpaint hwnd ))
+            (if (= WM_DESTROY msg)(quit) )
+        )                
+    ))
+    (register (app) 'HelloWin1' )
+    (create (app) 'HelloWin1')
+)
+0x0000001c7fb03480
+0x0000001c7fb03480
+any text or comment
+hdc = (hwnd hdc paint_struct)
